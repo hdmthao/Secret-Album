26a9c86d3b5bde5942fe667590f2411ce293c00b
itruonghai <nguyentruonghai141@gmail.com>
2020-08-23 01:14:39 +0700

    GridView

--- app/src/main/java/com/bigocoding/secretalbum/MainActivity.java
+++ app/src/main/java/com/bigocoding/secretalbum/MainActivity.java
@@ -2,11 +2,13 @@ package com.bigocoding.secretalbum;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ClipData;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -16,15 +18,25 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Photo> _mygallery ;
     private GridView _gridview ;
     private GridviewAdapter gridviewAdapter ;
+    private static final int  RESULT_CODE_PICKER_IMAGES = 9000;
+    int PICK_IMAGE_MULTIPLE = 1;
+    String imageEncoded;
+    List<String> imagesEncodedList;
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -58,78 +70,110 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MyString", "Haideptrai");
         verifyStoragePermissions(this);
         initComponent() ;
-        updateGridview() ;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.addphoto:
+                doSomething();
+                return true;
 
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
+    private void doSomething() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_IMAGE_MULTIPLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ArrayList<Photo> result;
+        result = new ArrayList<>();
+        try {
+            // When an Image is picked
+            if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                imagesEncodedList = new ArrayList<String>();
+                if(data.getData()!=null){
+
+                    Uri mImageUri=data.getData();
+
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(mImageUri,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imageEncoded  = cursor.getString(columnIndex);
+                    cursor.close();
+
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+                            // Get the cursor
+                            Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            imageEncoded  = cursor.getString(columnIndex);
+                            imagesEncodedList.add(imageEncoded);
+                            cursor.close();
+
+                        }
+                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+                        for (Uri i : mArrayUri){
+                            Log.d("LOG_Path ", i.toString()) ;
+                        }
+                        for (int i = 0 ; i < mArrayUri.size() ; i++){
+                            String name = mArrayUri.get(i).toString().substring(mArrayUri.get(i).toString().length()-4,mArrayUri.get(i).toString().length()-1);
+                            result.add(new Photo(name,mArrayUri.get(i))) ;
+                        }
+
+                    }
+
+                }
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+        _mygallery = result ;
+        Log.d("LOG_gallery",  String.valueOf(_mygallery.size()));
+        gridviewAdapter = new GridviewAdapter(MainActivity.this, R.layout.gridview_item, _mygallery) ;
+        _gridview.setAdapter(gridviewAdapter);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     private void initComponent() {
         _gridview = findViewById(R.id.gridview);
 
-    }
-    private void updateGridview() {
-        (new AsyncTaskLoadImage(this)).execute();
-    }
-    private class AsyncTaskLoadImage extends AsyncTask<Void, Void, ArrayList<Photo>> {
-        private ProgressDialog _dialog;
-
-        public AsyncTaskLoadImage(Activity activity) {
-            _dialog = new ProgressDialog(activity);
-        }
-
-        @Override
-        protected void onPreExecute() {
-            _dialog.setTitle("Loading");
-            _dialog.show();
-        }
-
-        @Override
-        protected ArrayList<Photo> doInBackground(Void... voids) {
-            // https://developer.android.com/training/data-storage/shared/media
-            ArrayList<Photo> results = new ArrayList<>();
-
-            String[] projection = new String[]{
-                    MediaStore.Images.Media._ID,
-                    MediaStore.Images.Media.DISPLAY_NAME
-            };
-            String sortOrder = MediaStore.Images.Media.DISPLAY_NAME + " ASC";
-
-            try (Cursor cursor = getApplicationContext().getContentResolver().query(
-                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    //MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    projection,
-                    null,
-                    null,
-                    sortOrder
-            )) {
-                // Cache column indices.
-                int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
-                int nameColumn =
-                        cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME);
-
-                while (cursor.moveToNext()) {
-                    long id = cursor.getLong(idColumn);
-                    String name = cursor.getString(nameColumn);
-
-                    Uri contentUri = ContentUris.withAppendedId(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-                    Photo item = new Photo(name,contentUri);
-                    results.add(item);
-                }
-            }
-            return results;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Photo> results) {
-            _mygallery = (ArrayList<Photo>) results;
-            gridviewAdapter = new GridviewAdapter(MainActivity.this,
-                    R.layout.gridview_item, _mygallery);
-            _gridview.setAdapter(gridviewAdapter);
-            if (_dialog.isShowing()) {
-                _dialog.dismiss();
-            }
-        }
     }
 
 
